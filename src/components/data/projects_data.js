6094caa522598ec9f7dbc215c32e724cfc75d249
Lorena C <lorena@eunknown.org>
2021-01-20 21:03:52 -0800

    update links and education

--- src/components/data/projects_data.js
+++ src/components/data/projects_data.js
@@ -1,52 +1,25 @@
-import tutor from "../../assets/icons/tutor.svg";
-import grocery from "../../assets/icons/grocery.svg";
+import tutor from '../../assets/icons/tutor.svg';
+import grocery from '../../assets/icons/grocery.svg';
 
-
-const data_projects =[
-    {
-       name: 'Tutor-Time',
-       image: tutor,
-       github_url: 'https://github.com/lorena57/Tutor-Time',
-       category: ['Rails', 'SQL', 'React', 'JavaScript'] 
-    },
-    {
-       name: 'My Groceries',
-       image: grocery,
-       github_url: 'https://github.com/lorena57/my_groceries_frontend',
-       category: ['JavaScript', 'Postgres'] 
-    },
-      {
-       name: 'My It',
-       image: grocery,
-       github_url: 'https://github.com/lorena57/my_groceries_frontend',
-       category: ['JavaScript', 'React', 'Rails'] 
-    },
-          {
-       name: 'My Item',
-       image: grocery,
-       github_url: 'https://github.com/lorena57/my_groceries_frontend',
-       category: ['React', 'Rails'] 
-    },
-          {
-       name: 'My Test',
-       image: grocery,
-       github_url: 'https://github.com/lorena57/my_groceries_frontend',
-       category: ['React', 'Rails'] 
-    },
-          {
-       name: 'My Testing',
-       image: grocery,
-       github_url: 'https://github.com/lorena57/my_groceries_frontend',
-       category: ['React', 'Rails'] 
-    },
-          {
-       name: 'My Time',
-       image: grocery,
-       github_url: 'https://github.com/lorena57/my_groceries_frontend',
-       category: ['React', 'Rails'] 
-    }
-
-
-]
+const data_projects = [
+  {
+    name: 'Tutor-Time',
+    image: tutor,
+    github_url: 'https://github.com/lorena57/Tutor-Time',
+    category: ['Rails', 'SQL'],
+  },
+  {
+    name: 'My Groceries',
+    image: grocery,
+    github_url: 'https://github.com/lorena57/my_groceries_frontend',
+    category: ['JavaScript', 'PostgreSQL', 'Rails'],
+  },
+  {
+    name: 'All In One',
+    image: grocery,
+    github_url: 'https://github.com/lorena57/all-in-one-again-front-end',
+    category: ['PostgreSQL', 'React', 'Rails'],
+  },
+];
 
 export default data_projects;
